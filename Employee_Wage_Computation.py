a3916f2216ef1e9a66e42d9d9295f3cd2057579b
shivammathur4 <shivamm.4aug@gmail.com>
2021-06-29 03:34:02 +0530

    Add: Code to to calculate daily wage with inclusion of part time hour

--- Employee_Wage_Computation.py
+++ Employee_Wage_Computation.py
@@ -10,10 +10,16 @@ import random
 print("Welcome to Employee Wage Computation Program")
 WAGE_PER_HOUR = 20
 FULL_DAY_HOUR = 8
-attendance = random.randint(0, 1)
+PART_TIME_HOUR = 4
+attendance = random.randint(0, 2)
 if attendance == 0:
     print("Absent")
+    employee_wage = 0
 elif attendance == 1:
     print("Present")
-employee_wage = attendance * WAGE_PER_HOUR * FULL_DAY_HOUR
+    employee_wage = WAGE_PER_HOUR * FULL_DAY_HOUR
+elif attendance == 2:
+    print("Part Time")
+    employee_wage = WAGE_PER_HOUR * PART_TIME_HOUR
+
 print("Employee Wage  is equal to {}".format(employee_wage))
