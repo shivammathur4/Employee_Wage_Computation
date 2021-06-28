bd4234f03db63a69cc3c019e5f118912a83573de
shivammathur4 <shivamm.4aug@gmail.com>
2021-06-29 03:40:26 +0530

    Add: Code to calculate the monthly wage of the employee

--- Employee_Wage_Computation.py
+++ Employee_Wage_Computation.py
@@ -12,15 +12,21 @@ print("Welcome to Employee Wage Computation Program")
 WAGE_PER_HOUR = 20
 FULL_DAY_HOUR = 8
 PART_TIME_HOUR = 4
-attendance = random.randint(0, 2)
-if attendance == 0:
-    print("Absent")
-    employee_wage = 0
-elif attendance == 1:
-    print("Present")
-    employee_wage = WAGE_PER_HOUR * FULL_DAY_HOUR
-elif attendance == 2:
-    print("Part Time")
-    employee_wage = WAGE_PER_HOUR * PART_TIME_HOUR
-
-print("Employee Wage  is equal to {}".format(employee_wage))
+employee_wage = 0
+days = 1
+total_wage = 0
+while days <= 20:
+    attendance = random.randint(0, 2)
+    if attendance == 0:
+        print("Absent")
+        employee_wage = 0
+    elif attendance == 1:
+        print("Present")
+        employee_wage = WAGE_PER_HOUR * FULL_DAY_HOUR
+    elif attendance == 2:
+        print("Part Time")
+        employee_wage = WAGE_PER_HOUR * PART_TIME_HOUR
+    print("Employee Wage for day {} is equal to {}".format(days, employee_wage))
+    days += 1
+    total_wage += employee_wage
+print("Total Wage of the employee {}".format(total_wage))
