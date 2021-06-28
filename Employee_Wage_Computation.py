7f1f7ae8f7fc7e35546cd1c3287c97ce5af0ae4c
shivammathur4 <shivamm.4aug@gmail.com>
2021-06-29 03:58:22 +0530

    Add: condition for days and hour

--- Employee_Wage_Computation.py
+++ Employee_Wage_Computation.py
@@ -7,26 +7,60 @@
 
 import random
 
-print("Welcome to Employee Wage Computation Program")
 
-WAGE_PER_HOUR = 20
-FULL_DAY_HOUR = 8
-PART_TIME_HOUR = 4
-employee_wage = 0
-days = 1
-total_wage = 0
-while days <= 20:
-    attendance = random.randint(0, 2)
-    if attendance == 0:
-        print("Absent")
+class EmployeeWageComputation:
+
+    print("Welcome to Employee Wage Computation Program")
+
+    def __init__(self):
+        self.WAGE_PER_HOUR = 20
+        self.FULL_DAY_HOUR = 8
+        self.PART_TIME_HOUR = 4
+
+    @staticmethod
+    def generate_random_attendance():
+        return random.randint(0, 2)
+
+    def check_employee_attendance(self):
+        '''
+    Description:
+        this function checks attendance .
+    Parameters:
+        No parameters.
+    Return:
+        return attendence for full and part time.
+    '''
+        attendance = self.generate_random_attendance()
+        switcher = {
+            0: 0,
+            1: self.FULL_DAY_HOUR,
+            2: self.PART_TIME_HOUR,
+        }
+        return switcher.get(attendance)
+
+    def calculate_employee_wage(self):
+         '''
+    Description:
+        this function calculate employee wage .
+    Parameters:
+        No parameters.
+    Return:
+        return total wage of employee.
+        '''
+        
+        days = 1
         employee_wage = 0
-    elif attendance == 1:
-        print("Present")
-        employee_wage = WAGE_PER_HOUR * FULL_DAY_HOUR
-    elif attendance == 2:
-        print("Part Time")
-        employee_wage = WAGE_PER_HOUR * PART_TIME_HOUR
-    print("Employee Wage for day {} is equal to {}".format(days, employee_wage))
-    days += 1
-    total_wage += employee_wage
-print("Total Wage of the employee {}".format(total_wage))
+        total_wage = 0
+        total_hours = 0
+        while days <= 20 and total_hours <= 100:
+            hours = self.check_employee_attendance()
+            employee_wage = self.WAGE_PER_HOUR * hours
+            print("Employee Wage for day {} is equal to {}".format(days, employee_wage))
+            total_wage += employee_wage
+            days += 1
+            total_hours +=hours
+        print("Total Wage of the employee {} and he worked for total {} hours".format(total_wage, total_hours))
+
+
+obj = EmployeeWageComputation()
+obj.calculate_employee_wage()
