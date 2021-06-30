896b3395ea4434da76fe8c90a7beb95bfad8ee03
shivammathur4 <shivamm.4aug@gmail.com>
2021-06-30 11:18:42 +0530

    Add: Give Daily wage

--- Employee_Wage_Computation.py
+++ Employee_Wage_Computation.py
@@ -12,16 +12,25 @@ class EmployeeWageComputation:
 
     print("Welcome to Employee Wage Computation Program")
 
-    def __init__(self):
-        self.WAGE_PER_HOUR = 20
-        self.FULL_DAY_HOUR = 8
-        self.PART_TIME_HOUR = 4
+    def __init__(self, COMPANY_NAME, WAGE_PER_HOUR, FULL_DAY_HOUR, PART_TIME_HOUR):
+        self.COMPANY_NAME = COMPANY_NAME
+        self.WAGE_PER_HOUR = WAGE_PER_HOUR
+        self.FULL_DAY_HOUR = FULL_DAY_HOUR
+        self.PART_TIME_HOUR = PART_TIME_HOUR
 
     @staticmethod
     def generate_random_attendance():
         return random.randint(0, 2)
 
     def check_employee_attendance(self):
+        '''
+    Description:
+        this function checks attendance .
+    Parameters:
+        No parameters.
+    Return:
+        return attendence for full and part time.
+         '''
         attendance = self.generate_random_attendance()
         switcher = {
             0: 0,
@@ -31,6 +40,15 @@ class EmployeeWageComputation:
         return switcher.get(attendance)
 
     def calculate_employee_wage(self):
+        '''
+    Description:
+        this function calculate employee wage .
+    Parameters:
+        No parameters.
+    Return:
+        return total wage of employee.
+        '''
+
         days = 1
         employee_wage = 0
         total_wage = 0
@@ -42,8 +60,7 @@ class EmployeeWageComputation:
             total_wage += employee_wage
             days += 1
             total_hours += hours
-        print("Total Wage of the employee {} and he worked for total {} hours".format(total_wage, total_hours))
+        print("Total Wage of the employee {} from the company {} and he worked for total {} hours".format(total_wage, self.COMPANY_NAME, total_hours,employee_wage))
 
-
-obj = EmployeeWageComputation()
-obj.calculate_employee_wage()
+        obj = EmployeeWageComputation()
+        obj.calculate_employee_wage()
